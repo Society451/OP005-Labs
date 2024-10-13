a8a594dbd1f5856939e7fd48c4347ce161997236
S451 <146851640+Society451@users.noreply.github.com>
2024-10-12 21:11:37 -0500

    Update calculate_means_and_averages.py

--- calculate_means_and_averages.py
+++ calculate_means_and_averages.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 # Data for 3ft trials (updated)
-trial_3ft = [0.385, 0.381, 0.322, 0.399, 0.397, 0.327]
+trial_3ft = [0.485, 0.421, 0.429, 0.415, 0.431, 0.427]
 average_3ft = np.mean(trial_3ft)
 std_dev_3ft = np.std(trial_3ft)
 
 # Data for 4ft trials
-trial_4ft = [0.439, 0.553, 0.524, 0.451, 0.472, 0.501]
+trial_4ft = [0.489, 0.553, 0.524, 0.451, 0.472, 0.501]
 average_4ft = np.mean(trial_4ft)
 std_dev_4ft = np.std(trial_4ft)
 
